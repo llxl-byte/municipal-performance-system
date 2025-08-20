d825e91f756274bda18d949ecec374eac13ff12b
dfpxqlx <477657920@qq.com>
2025-08-20 23:41:27 +0800

    解决500错误，原因在于vite.config的端口号8000改为8600，前后端不一致

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url' // 引入 fileURLToPath 和 URL
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // 使用新的方式处理路径别名
     },
   },
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8600',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
